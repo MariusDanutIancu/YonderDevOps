ab714ef39d6ca63c0f0b6904f5a96a34bd8de23e
Marius Danut Iancu <marius.danut94@outlook.com>
2018-07-11 17:40:49 +0300

    Updated repository.sh

--- Vagrant/provision_scripts/repository.sh
+++ Vagrant/provision_scripts/repository.sh
@@ -6,3 +6,22 @@ setenforce 0
 # install
 yum -y update
 yum -y install vim
+yum install createrepo
+yum install httpd
+
+mkdir -p /repos/centos/7/Packages
+mkdir -p /var/www/html/localrepo
+
+cat <<EOF > /etc/yum.repos.d/localrepo.repo
+[localrepo]
+name=yonder_devops Repository
+baseurl=file:///var/www/html/localrepo
+gpgcheck=0
+enabled=1
+EOF
+
+createrepo -v /var/www/html/localrepo
+createrepo --update /var/www/html/localrepo
+
+systemctl start httpd
+systemctl enable httpd
