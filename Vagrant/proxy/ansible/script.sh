a4df54cddea79378bf4f771ebe68111b53d1a123
Marius Danut Iancu <marius.danut94@outlook.com>
2018-07-26 12:51:38 +0300

    * updated proxy ansible tasks name * removed a proxy script * updated proxy firewalld script

--- Vagrant/proxy/ansible/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -u vagrant --private-key /home/marius/Yonder/Vagrant/.vagrant/machines/proxy/libvirt/private_key -i 10.143.20.2, /home/marius/Yonder/Vagrant/global/scripts/ansible/install.yml
-ansible-playbook -u vagrant --private-key /home/marius/Yonder/Vagrant/.vagrant/machines/proxy/libvirt/private_key -i 10.143.20.2, /home/marius/Yonder/Vagrant/global/users/ansible/users.yml
-ansible-playbook -u vagrant --private-key /home/marius/Yonder/Vagrant/.vagrant/machines/proxy/libvirt/private_key -i 10.143.20.2, configure_proxy.yml
--- Vagrant/proxy/scripts/proxy_firewalld.sh
+++ Vagrant/proxy/scripts/proxy_firewalld.sh
@@ -10,12 +10,11 @@ firewall-cmd --zone=yonder --permanent --add-service=dhcp
 firewall-cmd --zone=yonder --permanent --add-service=snmp
 firewall-cmd --zone=yonder --permanent --add-service=http
 firewall-cmd --zone=yonder --permanent --add-service=https
-
-firewall-cmd --zone=yonder --permanent --add-port=161/udp 
-
+	
 firewall-cmd --zone=yonder --permanent --change-interface=eth0
 firewall-cmd --zone=yonder --permanent --change-interface=eth1
 firewall-cmd --zone=yonder --permanent --change-interface=wlol
+
 firewall-cmd --set-default-zone=yonder
 
 systemctl restart network
